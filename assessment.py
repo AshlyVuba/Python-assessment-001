ff9d58672aee0dcdb95857945adf5e7ccf5755ff
Nhlakanipho Masilela <48583791+micanipho@users.noreply.github.com>
2025-11-24 12:26:24 +0200

    Add function definitions with docstrings in assessment.py
    This file contains several function definitions with docstrings outlining their intended functionality. Each function currently has a placeholder implementation.

---
+++ assessment.py
@@ -0,0 +1,53 @@
+
+def cube_number(number):
+    """
+    1. Receive a number.
+    2. Return that number raised to the power of 3.
+    """
+    # TODO: Write your code here
+    pass
+
+def check_even_or_odd(number):
+    """
+    1. Receive a number.
+    2. If even, return "Even".
+    3. If odd, return "Odd".
+    """
+    # TODO: Write your code here
+    pass
+
+def combine_names(first_name, last_name):
+    """
+    1. Receive two strings (first_name and last_name).
+    2. Return a single string in the format: "Last, First"
+    Example: combine_names("James", "Bond") -> "Bond, James"
+    """
+    # TODO: Write your code here
+    pass
+
+def get_last_item(my_list):
+    """
+    1. Receive a list.
+    2. Return the last item in that list (use negative indexing).
+    """
+    # TODO: Write your code here
+    pass
+
+def sum_all_numbers(numbers):
+    """
+    1. Receive a list of numbers (e.g., [1, 2, 3]).
+    2. Return the sum of all numbers in the list.
+    Hint: You can use a 'for' loop or the built-in sum() function.
+    """
+    # TODO: Write your code here
+    pass
+
+def get_country_code(database, country):
+    """
+    1. Receive a dictionary (database) and a key (country).
+    2. Return the value associated with that key.
+    Example input: {'ZA': 'South Africa', 'US': 'USA'}, 'ZA'
+    Example output: 'South Africa'
+    """
+    # TODO: Write your code here
+    pass
